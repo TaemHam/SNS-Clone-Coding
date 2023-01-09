0d85f9583ed84aa545e92d0940eef84eccb3b48b
TaemHam <tk4003950@gmail.com>
2023-01-09 10:47:15 +0900

    #1 - 스프링부트 프로젝트 시작
    스프링 이니셜라이저를 통해 스프링 부트 프로젝트를 시작함.
    
    개발 환경:
    * Java 11
    * Springboot 2.6.7
    
    의존성:
    * Spring Web WEB
    * Lombok DEVELOPER TOOLS
    * Spring Security SECURITY
    * PostgreSQL Driver SQL
    * Spring Data JPA

---
+++ src/test/java/com/practice/sns/SnsCloneCodingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCloneCodingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
